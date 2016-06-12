d618603d8d8e0a972a1ce941cf7f5a53130d08b9
Patrik Sandahl <kosmoskatten.software@gmail.com>
2016-06-12 21:32:34 +0200

    Automatic start.

--- ghost-lang-client/start.sh
+++ ghost-lang-client/start.sh
@@ -13,42 +13,47 @@ function assertStatus {
 
 # Anyway, isp run on 213 network and clients on the 217 network.
 
+TUN=tun0
+
 echo -n "Creating a tun device named tun0 ... "
-ip tuntap add mode tun tun0
+ip tuntap add mode tun ${TUN}
 assertStatus
 echo "Done!"
 
 echo -n "Activating tun0 ... "
-ip link set tun0 up
+ip link set ${TUN} up
 assertStatus
 echo "Done!"
 
 echo -n "Creating (client) interface address 217.0.0.1 on tun ... "
-ip addr add 217.0.0.1/24 dev tun0
+ip addr add 217.0.0.1/24 dev ${TUN}
 assertStatus
 echo "Done!"
 
 echo -n "Creating client address 217.0.0.2 on tun ... "
-ip addr add 217.0.0.2/32 dev tun0
+ip addr add 217.0.0.2/32 dev ${TUN}
 assertStatus
 echo "Done!"
 
 echo -n "Creating client address 217.0.0.3 on tun ... "
-ip addr add 217.0.0.3/32 dev tun0
+ip addr add 217.0.0.3/32 dev ${TUN}
 assertStatus
 echo "Done!"
 
 echo -n "Creating client address 217.0.0.4 on tun ... "
-ip addr add 217.0.0.4/32 dev tun0
+ip addr add 217.0.0.4/32 dev ${TUN}
 assertStatus
 echo "Done!"
 
 echo -n "Creating interface address 217.0.0.5 on tun ... "
-ip addr add 217.0.0.5/32 dev tun0
+ip addr add 217.0.0.5/32 dev ${TUN}
 assertStatus
 echo "Done!"
 
 echo -n "Routing traffic to 213.0.0.0/24 to tun0 ... "
-ip route add 213.0.0.0/24 dev tun0 src 217.0.0.1
+ip route add 213.0.0.0/24 dev ${TUN} src 217.0.0.1
 assertStatus
 echo "Done!"
+
+./tun-plugs client ${TUN} ghost-server 8000 &
+./ghost-node 80
--- ghost-lang-server/start.sh
+++ ghost-lang-server/start.sh
@@ -12,22 +12,28 @@ function assertStatus {
 
 # Anyway, isp run on 213 network and clients on the 217 network.
 
+TUN=tun0
+HOST=`hostname -i`
+
 echo -n "Creating a tun device named tun0 ... "
-ip tuntap add mode tun tun0
+ip tuntap add mode tun ${TUN}
 assertStatus
 echo "Done!"
 
 echo -n "Activating tun0 ... "
-ip link set tun0 up
+ip link set ${TUN} up
 assertStatus
 echo "Done!"
 
 echo -n "Creating (ISP) interface address 213.0.0.1 on tun ... "
-ip addr add 213.0.0.1/24 dev tun0
+ip addr add 213.0.0.1/24 dev ${TUN}
 assertStatus
 echo "Done!"
 
 echo -n "Routing traffic to 217.0.0.0/24 to tun0 ... "
-ip route add 217.0.0.0/24 dev tun0 src 213.0.0.1
+ip route add 217.0.0.0/24 dev ${TUN} src 213.0.0.1
 assertStatus
 echo "Done!"
+
+./tun-plugs server ${TUN} ${HOST} 8000 &
+./ghost-server 213.0.0.1 80
